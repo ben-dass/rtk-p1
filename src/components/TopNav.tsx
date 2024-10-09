586028758eaaf7ded43a812775cbabe701a1cd66
Benjamin Dass <65924137+ben-dass@users.noreply.github.com>
2024-10-09 14:18:22 -0400

    Initial commit

---
+++ src/components/TopNav.tsx
@@ -0,0 +1,14 @@
+const TopNav = () => {
+	return (
+		<div className="mb-[2.5rem] mt-[1.5rem] flex h-full w-full flex-row justify-between rounded-lg bg-gray-400 bg-opacity-20 pb-[0.5rem] pt-[0.5rem] outline outline-[0.11rem] outline-black drop-shadow-lg backdrop-blur-lg">
+			<div className="ml-2 font-bold uppercase">
+				React Starter Template
+			</div>
+			<div className="mr-2 flex items-center text-xs">
+				<em>Benjamin Dass</em>
+			</div>
+		</div>
+	);
+};
+
+export default TopNav;
